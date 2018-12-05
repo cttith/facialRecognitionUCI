9fb2539af43ef964feef0f4d9b45498d6cd904bf
cttith <cttith@gmail.com>
2018-12-04 18:16:14 -0600

    tweaking NN model

--- modelCreation.R
+++ modelCreation.R
@@ -25,10 +25,6 @@ compile(emotionModel,
         loss='categorical_crossentropy',
         metrics = 'accuracy')
 
-
-
-
-
 # Fitting model
 emotionHistory <- fit(emotionModel,
                       train_final_images, train_oneHot,
@@ -38,3 +34,44 @@ emotionHistory <- fit(emotionModel,
 
 score <- evaluate(emotionModel,
                   test_final_images, test_oneHot)
+
+
+
+########### SETTING UP NEW MODEL
+model<-keras_model_sequential()
+#configuring the Model
+model %>%  
+  #defining a 2-D convolution layer
+  
+  layer_conv_2d(filter=32,kernel_size=c(3,3),padding="same",input_shape=c(32,30,1) ) %>%  
+  layer_activation("relu") %>%  
+  #another 2-D convolution layer
+  
+  layer_conv_2d(filter=32 ,kernel_size=c(3,3))  %>%  layer_activation("relu") %>%
+  #Defining a Pooling layer which reduces the dimentions of the #features map and reduces the computational complexity of the model
+  layer_max_pooling_2d(pool_size=c(2,2)) %>%  
+  #dropout layer to avoid overfitting
+  layer_dropout(0.25) %>%
+  layer_conv_2d(filter=32 , kernel_size=c(3,3),padding="same") %>% layer_activation("relu") %>%  layer_conv_2d(filter=32,kernel_size=c(3,3) ) %>%  layer_activation("relu") %>%  
+  layer_max_pooling_2d(pool_size=c(2,2)) %>%  
+  layer_dropout(0.25) %>%
+  #flatten the input  
+  layer_flatten() %>%  
+  layer_dense(512) %>%  
+  layer_activation("relu") %>%  
+  layer_dropout(0.5) %>%  
+  #output layer-10 classes-10 units  
+  layer_dense(4) %>%  
+  #applying softmax nonlinear activation function to the output layer #to calculate cross-entropy  
+  layer_activation("softmax") 
+#for computing Probabilities of classes-"logit(log probabilities)
+
+
+compile(model,
+        optimizer ='adam',
+        loss='categorical_crossentropy',
+        metrics = 'accuracy')
+model2 <- fit(model,
+                      train_images_new_model, train_oneHot,
+                      validation_split = 0.2, batch_size=32,
+                      epochs = 500)
--- processData.R
+++ processData.R
@@ -19,6 +19,8 @@ test_images <- NULL
 test_images <- pixelMap[-train]
 test_emotion <- emotionLabel[-train]
 
+train_images_new_model <- array(dim=c(numTrainImages, 32, 30,1))
+for( j in 1:numTrainImages) train_images_new_model[j,,,] <- train_images[[j]]
 
 #####UP TO HERE, IT'S CORRECT, A1
 
@@ -29,7 +31,7 @@ for (j in 1:numTrainImages)  train_final_images[j,,]  <- train_images[[j]]
 
 numTestImages = length(test_images)
 test_final_images <- array(dim=c(numTestImages, 32, 30))
-for (j in 1:numTestImages)  test_final_images[j,,]  <- train_images[[j]]
+for (j in 1:numTestImages)  test_final_images[j,,]  <- test_images[[j]]
 
 
 # one hot encoding
